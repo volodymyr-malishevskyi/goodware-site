d783247c4eeaf9a9f1096832dc36452217608077
Volodymyr Malishevskyi <boomcrouser@gmail.com>
2025-03-11 13:27:14 +0200

    Add BRDO brand and improve adaptivity

--- custom/brands-slider/index.js
+++ custom/brands-slider/index.js
@@ -3,11 +3,12 @@ const template = `
     <div class="brands">
       <div class="brands__slide">
       <img src="/custom/brands-slider/Logo MinMolodi.png" alt="Міністерство Молоді" />
-      <img src="/custom/brands-slider/Logo MinEconomy.png" alt="Міністерство Економіки" />
       <img src="/custom/brands-slider/Logo MinDovkilya.png" alt="Міністерство Довкілля" />
+      <img src="/custom/brands-slider/Logo MinEconomy.png" alt="Міністерство Економіки" />
         <img src="/custom/brands-slider/Logo Diia.png" alt="Дія" />
         <img src="/custom/brands-slider/Logo MinDigital.png" alt="МінЦифра" />
         <img src="/custom/brands-slider/Logo Kitsoft.png" alt="KITSOFT" />
+        <img src="/custom/brands-slider/Logo BRDO.png" alt="БРДО" />
       </div>
   </div>
 </div>
